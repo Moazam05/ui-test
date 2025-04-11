2dfc0e92369d5631ab2c94f1a4ecd81de5dd7046
Moazam05 <salmanmoazam08@gmail.com>
2025-04-11 16:00:02 +0500

    assets refactor

---
+++ src/assets/images/index.js
@@ -0,0 +1,37 @@
+import ActivityFourImg from "./activity-four.png";
+import ActivityOneImg from "./activity-one.png";
+import ActivityThreeImg from "./activity-three.png";
+import ActivityTwoImg from "./activity-two.png";
+import AppIconMobileImg from "./appicon-mobile.png";
+import AppIconImg from "./appicon.png";
+import ChairmanImg from "./chairman.png";
+import CultureSvg from "./culture.svg";
+import HeroImg from "./hero.png";
+import PartnerFourSvg from "./partner-four.svg";
+import PartnerOneSvg from "./partner-one.svg";
+import PartnerThreeSvg from "./partner-three.svg";
+import PartnerTwoSvg from "./partner-two.svg";
+import PersonOneImg from "./person-one.png";
+import PersonThreeImg from "./person-three.png";
+import PersonTwoImg from "./person-two.png";
+
+const images = {
+  ActivityFourImg,
+  ActivityOneImg,
+  ActivityThreeImg,
+  ActivityTwoImg,
+  AppIconMobileImg,
+  AppIconImg,
+  ChairmanImg,
+  CultureSvg,
+  HeroImg,
+  PartnerFourSvg,
+  PartnerOneSvg,
+  PartnerThreeSvg,
+  PartnerTwoSvg,
+  PersonOneImg,
+  PersonThreeImg,
+  PersonTwoImg,
+};
+
+export default images;
